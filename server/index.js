230e5b02afc1d6d7225a124f9ace51d8f8305164
Hein Wai Yan Htet <heinwaiyanhtet20042020@gmail.com>
2024-03-30 00:34:56 +0630

    mongoDb

--- server/index.js
+++ server/index.js
@@ -3,15 +3,30 @@ const fastify = Fastify({
   logger: true
 })
 
+
 // Declare a route
 fastify.get('/', async function handler (request, reply) {
+
   return { hello: 'world' }
+
+})
+
+fastify.post('/contact',async function handler (request,reply) {
+
+    const { name, email, message } = request.body;
+
+    console.log('Received contact form submission:', { name, email, message });
+
+    reply.code(200).send({ success: true });
+    
 })
 
 // Run the server!
-try {
-    await fastify.listen({ port: 3001 })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+
+try 
+{
+await fastify.listen({ port: 3001 })
+} catch (err) {
+fastify.log.error(err)
+process.exit(1)
+}
