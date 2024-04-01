d5971906ae028790980385de503d919d9f391672
Hein Wai Yan Htet <heinwaiyanhtet20042020@gmail.com>
2024-04-02 00:48:31 +0630

    server hbs started

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,3 @@
-
 import { MongoClient } from 'mongodb';
 
 const uri =  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.2";
--- server/index.js
+++ server/index.js
@@ -1,53 +1,92 @@
 import Fastify from 'fastify';
 import mongodbClient from './config/database.js';
+import helmet from '@fastify/helmet';
+import fastifyView from '@fastify/view';
+import Handlebars from 'handlebars';
 
 const fastify = Fastify({
   logger: true
 })
 
+fastify.register(helmet);
+
+
+fastify.register(fastifyView, {
+    engine: {
+      handlebars: Handlebars,
+    },
+    layout: "/views/layouts/main.hbs",
+    options: {
+      partials: {
+        head: '/views/partials/head.hbs',
+        header: '/views/partials/header.hbs',
+        footer: '/views/partials/footer.hbs'
+      }
+    } 
+});
+
+
+// fastify.register(pointOfView, {
+//   engine: {
+//     handlebars: require('handlebars')
+//   },
+//   includeViewExtension: true,
+//   options: {
+//     partials: {
+//       head: '/views/partials/head.hbs',
+//       header: '/views/partials/header.hbs',
+//       footer: '/views/partials/footer.hbs'
+//     }
+//   } 
+// });
+
 
 // Declare a route
+
 fastify.get('/', async function handler (request, reply) {
-
-  return { hello: 'world' }
-
+    return { hello: 'world' }
 })
 
-async function insertData(name,email,message){
-     try 
-      {
-          
-          await mongodbClient.connect();
 
-          const database = mongodbClient.db("contact"); 
-          const collection = database.collection("contact");
-          await collection.insertOne({ name, email, message });
-          console.log('Contact form data inserted into MongoDB:', { name, email, message });
-          
-      } 
-      catch (e) {
-          console.error('Error inserting contact form data into MongoDB:', error);
-      } 
-      finally {
-          await mongodbClient.close();
-      }
+
+
+
+async function insertData(name,email,message){
+    try 
+    {
+        await mongodbClient.connect();
+        const database = mongodbClient.db("contact"); 
+        const collection = database.collection("contact");
+        await collection.insertOne({ name, email, message });
+        console.log('Contact form data inserted into MongoDB:', { name, email, message });
+    } 
+    catch (e) {
+        console.error('Error inserting contact form data into MongoDB:', error);
+    } 
+    finally {
+        await mongodbClient.close();
+    }
 
 }
 
 fastify.post('/contact',async function handler (request,reply) {
 
   try {
-    
+
     const { name, email, message } = request.body;
-
     await insertData(name,email,message);       
-
     reply.code(200).send({ success: true });
 
   } catch (error) {
     console.log("error occur",error);
   }
     
+})
+
+fastify.get('/dashboard',(request,reply) => {
+
+   reply.view('/views/layouts/main', {});
+
 })
 
 
@@ -58,7 +97,8 @@ try
   await fastify.listen({ port: 3000 })
 
 } catch (err) {
-  
-fastify.log.error(err)
-process.exit(1)
+
+  fastify.log.error(err)
+  process.exit(1)
+
 }
